0ecb6e7bc921ead517eb9a8288a8beb56f90852f
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-04-07 09:47:27 +0300

    Update harjutus3.gd

--- harjutus3.gd
+++ harjutus3.gd
@@ -2,11 +2,18 @@ extends Node
 var rng = RandomNumberGenerator.new()
 func _ready():
 	var vastaseelud = 100
-	var elud = 100
-	rng.randomize()
-	var rand = rng.randf_range(8,15)
+	var vastaseelud2 = 100
 	while vastaseelud > 0:
-		vastaseelud - rand
-		print("Hit: ",rand)
-		print("Life: ",vastaseelud)
-		
+		rng.randomize()
+		var rand = round(rng.randf_range(8,15))
+		rng.randomize()
+		var rando = round(rng.randf_range(8,15))
+		vastaseelud -= rand
+		vastaseelud2 -= rando
+		print("P1 Hit: ",rand, " P1 Life: ",vastaseelud)
+		print("P2 Hit: ",rando, " P2 Life: ",vastaseelud2)
+		if vastaseelud <= 0:
+			print("P2 võitis")
+		else:
+			if vastaseelud2 <= 0:
+				print("P1 võitis")
