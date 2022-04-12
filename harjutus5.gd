98914817e8d43d05f4f4d4c9101e985185539a4b
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-04-12 14:18:38 +0300

    Update harjutus5.gd

--- harjutus5.gd
+++ harjutus5.gd
@@ -16,15 +16,16 @@ func _ready():
 	too()
 
 
-var punktid = [7,28,64,51,81,40,21,73,34,98,39,17,33,85,35,44]
+func punkt():
+	var punktid = [7,28,64,51,81,40,21,73,34,98,39,17,33,85,35,44]
 
 
-
-if punktid > 90:
-	print(punktid, " - 5")
-elif punktid > 75:
-	print(punktid, " - 4")
-elif punktid > 50:
-	print(punktid, " - 3")
-else:
-	print(punktid, " - 2")
+for i in punktid:
+	if i > 90:
+		print(punktid"p", " - 5")
+	elif i > 75:
+		print(punktid"p", " - 4")
+	elif i > 50:
+		print(punktid"p", " - 3")
+	else:
+		print(punktid"p", " - 2")
