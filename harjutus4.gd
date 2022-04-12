aedc18ba1f339b733a3a1232db5b77a4b7bb0119
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-04-12 09:57:24 +0300

    Update harjutus4.gd

--- harjutus4.gd
+++ harjutus4.gd
@@ -1,17 +1,14 @@
 extends Node
 
 func _ready():
-	var nimed = {"Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin",
-"Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins",
-"Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey",
-"Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"}
+	var nimed = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin", "Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins", "Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey", "Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
 
 
-nimed.sort()
-var size = nimed.size()
-print("Massiivi suurus: ",size)
-nimed.append("Lauri")
-nimed.erase("Reyes")
+	nimed.sort()
+	var size = nimed.size()
+	print("Massiivi suurus: ",size)
+	nimed.append("Lauri")
+	nimed.erase("Reyes")
 
-for i in nimed:
+	for i in nimed:
 		 print(nimed[i])
