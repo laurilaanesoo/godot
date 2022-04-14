8a11ea9988b2fbe9bfc2b7bfb15c6414df280feb
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-04-14 10:32:17 +0300

    Update harjutus6.gd

--- harjutus6.gd
+++ harjutus6.gd
@@ -3,7 +3,9 @@ extends Node
 var vaenlane = 100
 var salv = 5
 var skoor = 0
+var lask = 0
 var pihtas = bool(randi() % 2)
+var eff = 100
 var rng = RandomNumberGenerator.new()
 func _ready():
 	print("----------------------------------")
@@ -18,10 +20,26 @@ func _process(_delta):
 			var suv = rng.randi_range(8,12)
 			vaenlane -= suv
 			print("Vaenlase elud: ", vaenlane)
+			skoor += 5
+			salv -= 1
+			lask += 1
+			
+			print("Salves: ", salv)
 		else:
 			print('lasid mööda')
+		if pihtas == false:
+			eff = 100 - 10
+		if vaenlane <= 0:
+			print("------------Mäng läbi------------------")	
+			get_tree().quit()
+			print("Skoor: ", skoor)
+			print("Laskude arv: ", lask)
+			print("Effektiivsus: ", eff)
 
 		
-		if Input.is_action_just_pressed("lae"):
-			print("laen")
-			salv = 5
+	if Input.is_action_just_pressed("lae"):
+		print("laen")
+		salv = 5
+		
+	
+	
